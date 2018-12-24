71b46890d3493abb78179a76e94a186952efbc47
Geng Zhang <zhanggeng.zg@antfin.com>
2018-12-24 21:11:09 +0800

    Add shell and config.

---
+++ sync-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+shellDir=$(cd "$(dirname "$0")"; pwd)
+repo_path=${shellDir}
+
+echo ">>> Current path is ${repo_path}"
+for line in `cat ./config.txt`
+do
+{
+    if [ "${line:0:1}" == "#" ]; then
+        continue
+    else
+        echo $line
+        cd ${repo_path}
+        array=(${line//,/ }) 
+        origin_repo=(${array[0]})
+        fork_repo=(${array[1]})
+        sh ./sync-repo.sh ${origin_repo} ${fork_repo}
+    fi
+}&
+done
+wait
---
+++ sync-repo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Sync one repo 
+origin_repo=$1
+fork_repo=$2
+echo "------------"
+echo ">>>  sync from ${origin_repo} to ${fork_repo}"
+
+fork_path=${fork_repo/:/\/}
+echo ">>>  fork path is ${fork_path} "
+if [ ! -d ${fork_path} ]; then
+    # git clone git@github.com:sofastack/sofa-rpc github.com/sofastack/sofa-rpc
+    git clone "git@${fork_repo}" ${fork_path}
+fi
+cd ${fork_path}
+git remote add upstream "git@${origin_repo}"
+git remote -v
+git fetch -p --all
+
+git merge upstream/master -q
+# 提交到fork master
+git push origin master
+echo "------------"
+echo 
+
