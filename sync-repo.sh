42f38f5bf1b59a1b92cfa77ede8ce2c5309a8abb
Geng Zhang <ujjboy@qq.com>
2019-03-07 14:49:53 +0800

    change to mirror

--- sync-repo.sh
+++ sync-repo.sh
@@ -3,23 +3,16 @@
 origin_repo=$1
 fork_repo=$2
 echo "------------"
-echo ">>>  sync from ${origin_repo} to ${fork_repo}"
+echo ">>>  sync from ${origin_repo} to ${origin_repo}"
 
-fork_path=${fork_repo/:/\/}
-echo ">>>  fork path is ${fork_path} "
-if [ ! -d ${fork_path} ]; then
-    # git clone git@github.com:sofastack/sofa-rpc github.com/sofastack/sofa-rpc
-    git clone "git@${fork_repo}" ${fork_path}
+origin_path=${origin_repo/:/\/}
+echo ">>>  origin path is ${origin_path} "
+if [ ! -d ${origin_path} ]; then
+    # git clone git@github.com:alipay/sofa-rpc github.com/alipay/sofa-rpc
+    git clone "git@${origin_repo}" ${origin_path}
 fi
-cd ${fork_path}
-git remote add upstream "git@${origin_repo}"
-git remote -v
+cd ${origin_path}
 git fetch -p --all
-
-git merge upstream/master -q
-# 提交到fork master
-git push origin master
-git push origin --tags
-echo "------------"
-echo 
-
+echo ">>>  push mirror to git@${fork_repo}"
+git push --mirror git@${fork_repo}
+echo
